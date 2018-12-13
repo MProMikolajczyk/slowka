fc45084a0464550c29581291e93243c8b7cf1aa9
mmpro <marekmikolajczyk87@gmail.com>
2018-12-13 20:42:20 +0100

    stworzono moduly do sprawdzania slowa + wersja polsko- angielska. +testy

--- slowo.py
+++ slowo.py
@@ -10,13 +10,6 @@ import random
 
 words_eng = ['red','blue','green']
 words_pol = ['czerwony','niebieski','zielony']
-random_words_eng = list() #lista slowek  angielskich wybranych losowo
-random_words_pol = list() #lista slowek poslich wybranych losowo
-
-random_words_eng.append(random.choice(words_eng)) #dodanie do zbioru losowego angielskiego slowka
-random_words_pol.append(random.choice(words_pol)) #dodanie do zbioru losowego posliego slowka
-
-index_random_words = words_eng.index(random_words_eng[0]) #index losowego angielskiego slowka
 
 '''zaminienia slowo wpowadzone na liste znakow'''
 def list_letter(word):
@@ -25,7 +18,7 @@ def list_letter(word):
 
 '''#spr. dlugosc podannego slowa ze slowem ze zbioru. 
 Wyrównuje ich dlugosc, wstawiaja lub usuwajac zanki ze slowa'''
-def check_len_word(list_words):
+def check_len_word(list_words,find_word):
     list_find_word=list(list_letter(find_word))
     if len(list_words[index_random_words]) == len(list_find_word):
         return list_find_word
@@ -36,26 +29,45 @@ def check_len_word(list_words):
             list_find_word.pop(len(list_find_word)-1)
     return list_find_word
 
-'''spr znaki w spowie uzytym i wpisamyn. 
+'''spr znaki w słowie uzytym i wpisamyn. 
 Tworzy liste ze znakami poprawnymi. 
 Źle wpisane znaki zamienia na '-' '''
-def check_word(lang_list_words):
+def check_word(lang_list_words,find_word):
     list_char_word = list()
     for i in range(len(lang_list_words[index_random_words])):
-        if lang_list_words[index_random_words][i] == check_len_word(lang_list_words)[i]:
-            list_char_word.append(check_len_word(lang_list_words)[i])
-
+        if lang_list_words[index_random_words][i] == check_len_word(lang_list_words,find_word)[i]:
+            list_char_word.append(check_len_word(lang_list_words,find_word)[i])
         else:
             list_char_word.append('-')
     return list_char_word
 
+'''Wybór opcji językowej'''
+def select_lang_version(lang_version_list):
+    if lang_version_list == words_pol: #spr warunku jezykowego (odpoweidz po polsku)
+        question_lang_word = random_words_eng
+        return question_lang_word
+    elif lang_version_list == words_eng: #spr warunku jezykowego (odpoweidz po angielsku)
+        question_lang_word = random_words_pol
+        return question_lang_word
 
+''' Pęta az to wpisania pożądanego słowa'''
+def loop_looking_word(lang_version_list):
+    select_lang_version(lang_version_list)
+    find_word = ''
+    while find_word != lang_version_list[index_random_words]:
+        find_word = input(select_lang_version(lang_version_list)[0] + ' = ' + str(check_word(lang_version_list,find_word))) #szukane (wpisywane) slowo
+        print('Podano : ' + find_word)  # wydruk poszukiwanego slowaP
+        if find_word == lang_version_list[index_random_words]:
+            print('dobrze')
+            break
+        elif find_word == 'n': #po wpisanu n pokazuje slowo szukane
+            print(select_lang_version(lang_version_list)[0] + ' = ' + lang_version_list[index_random_words])  # słówko ang i znaczenie polskie
+            break
 
-
-
-
-'''Wyduki funkkcji +mechanika'''
-find_word = input(random_words_eng[0] + ' = ' +'-'*len(words_pol[index_random_words])) # wpisywane slowo
-print("".join(check_word(words_pol))) #wydruk słowa wpisanego po sprawdzeniu dligosci i podmienieniu niewłaściwych lister
-print('Podano : '+ find_word) #wydruk poszukiwanego slowa
-print(random_words_eng[0] + ' = ' + words_pol[index_random_words]) #słówko ang i znaczenie polskie
+while True:
+    index_random_words=random.randint(0,len(words_eng)-1)
+    random_words_eng = list() #lista slowek  angielskich wybranych losowo
+    random_words_pol = list() #lista slowek poslich wybranych losowo
+    random_words_eng.append(words_eng[index_random_words])  #dodanie do zbioru losowego angielskiego slowka
+    random_words_pol.append(words_pol[index_random_words]) #dodanie do zbioru losowego posliego slowka
+    loop_looking_word(words_eng) #opdowiedź po polsku, tytaj wybiera sie jezyk w jakim chce sie odpowiadać.
--- test_slowo.py
+++ test_slowo.py
@@ -2,24 +2,35 @@
 import unittest
 import slowo
 
-slowo.index_random_words=1 # stały wybór brak prawdopodobieństwa. Zawsze sprwdzaj dla 1 w zbiorze
+slowo.index_random_words=0
 
 class Testslowo(unittest.TestCase):
-
     def test_list_letter(self):
         self.assertEqual(list(slowo.list_letter('big')),['b','i','g'])
         self.assertEqual(list(slowo.list_letter('data')), ['d', 'a', 't','a'])
         self.assertEqual(list(slowo.list_letter('')), [])
 
     def test_check_len_word(self):
-        self.assertEqual(len(slowo.check_len_word(['zbc', 'dsaa', 'cfres'])),
+        self.assertEqual(len(slowo.check_len_word(['zbc', 'dsaa', 'cfres'],'')),
                          len(['a-b','--ca','--abc'][slowo.index_random_words]))
 
     def test_check_word(self):
-        self.assertEqual(len(slowo.check_word(['czerwony','niebieski','zielony'])),
-                         len(['-------','---------','-------'][slowo.index_random_words]))
-        self.assertEqual(len(slowo.check_word(['red','blue','green'])),
+        self.assertEqual(len(slowo.check_word(['bbarwony','bbubieski','ccalony'],'')),
+                         len(['--------','---------','-------'][slowo.index_random_words]))
+        self.assertEqual(len(slowo.check_word(['red','blue','green'],'')),
                          len(['---','----','-----'][slowo.index_random_words]))
+        self.assertEqual(len(slowo.check_word(['bbarwony', 'bbubieski', 'ccalony'], 'bb------')),
+                         len(['bb------', '---------', '-------'][slowo.index_random_words]))
+
+    def test_select_lang_version(self):
+        self.assertEqual(slowo.select_lang_version(slowo.words_pol),slowo.random_words_eng)
+        self.assertEqual(slowo.select_lang_version(slowo.words_eng),slowo.random_words_pol)
+
+    def test_loop_looking_word(self):
+        self.assertEqual(([['zarwony'],['iubieski'],['ialony']][slowo.index_random_words]),
+                         [['zarwony'],['iubieski'],['ialony']][slowo.index_random_words])
+        self.assertEqual(([['ad'], ['loe'], ['rean']][slowo.index_random_words]),
+                         [['ad'], ['loe'], ['rean']][slowo.index_random_words])
 
 
 if __name__ == '__main__':
